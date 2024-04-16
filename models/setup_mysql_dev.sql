7278a702eeae5e8b6d12222f04f3d3932fb846a0
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-04-16 15:54:49 +0200

    3. MySQL setup development

---
+++ models/setup_mysql_dev.sql
@@ -0,0 +1,9 @@
+-- Setup MySQL
+-- Create DB manadatory Data
+CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
+-- Create User
+CREATE IF NOT EXISTSUSER `hbnb_dev` @`localhost` IDENTIFIED BY `hbnb_dev_pwd`;
+-- Grant all privileges
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev' @'localhost';
+-- Grant SELECT privilege
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev' @'localhost';

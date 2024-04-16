f5b8f6ae8a12b2f9f43fbda8d327fd7871391dba
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-04-17 01:49:25 +0200

    2. Console improvements

--- console.py
+++ console.py
@@ -123,38 +123,39 @@ class HBNBCommand(cmd.Cmd):
         """Overrides the emptyline method of CMD"""
         pass
 
-    def do_create(self, line):
-        """Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
-        """
-        try:
-            if not line:
-                raise SyntaxError()
-            my_list = line.split(" ")
+    def do_create(self, args):
+        """Create an object of any class"""
 
-            kwargs = {}
-            for i in range(1, len(my_list)):
-                key, value = tuple(my_list[i].split("="))
-                if value[0] == '"':
-                    value = value.strip('"').replace("_", " ")
-                else:
-                    try:
-                        value = eval(value)
-                    except (SyntaxError, NameError):
-                        continue
-                kwargs[key] = value
-
-            if kwargs == {}:
-                obj = eval(my_list[0])()
-            else:
-                obj = eval(my_list[0])(**kwargs)
-                storage.new(obj)
-            print(obj.id)
-            obj.save()
-
-        except SyntaxError:
+        if not args:
             print("** class name missing **")
-        except NameError:
+            return
+        all_arg = args.split(' ')
+        current_class = all_arg.pop(0)
+        if current_class not in HBNBCommand.classes:
             print("** class doesn't exist **")
+            return
+
+        map_arg = {}
+        for a in all_arg:
+            try:
+
+                k, v = a.split('=')
+                k = k.strip()
+                v = v.strip()
+                if v.startswith('"') and v.endswith('"'):
+                    v = v[1:-1].replace('\"', '"').replace("_", " ")
+                elif '.' in v:
+                    v = float(v)
+                else:
+                    v = int(v)
+                map_arg[k] = v
+
+            except ValueError:
+                continue
+        new_instance = HBNBCommand.classes[current_class](**map_arg)
+        storage.save()
+        print(new_instance.id)
+        storage.save()
 
     def help_create(self):
         """Help information for the create method"""

05a4ece19e91ac2732f77fa10f3b8e6c8d7578b3
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-04-16 14:45:13 +0200

    2. Console improvements

--- models/user.py
+++ models/user.py
@@ -5,7 +5,18 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(
+        self, email="",
+        password="", first_name="", last_name="", *args, **kwarg
+    ):
+        super().__init__(*args, **kwarg)
+        self.email = email
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name

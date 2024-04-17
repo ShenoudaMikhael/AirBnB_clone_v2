f5a1556f5dee79d7c8254989ac80c9780bb4a82a
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-04-17 19:26:17 +0200

    FIX

--- models/amenity.py
+++ models/amenity.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
+    """This class defines a Amenity by various attributes"""
 
     def __init__(self, *args, name="", **kwargs):
         super().__init__(*args, **kwargs)

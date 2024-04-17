ec47b05f00031796396d0a86e44c0f3b2252c1f6
Salsabil <salsabeelahmed323@gmail.com>
2024-04-17 15:12:57 +0200

    8. DBStorage - Place

--- models/place.py
+++ models/place.py
@@ -1,38 +1,36 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.review import Review
+from sqlalchemy import Column, Float, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """A place to stay"""
 
-    def __init__(
-        self,
-        city_id=0,
-        user_id=0,
-        name="",
-        description="",
-        number_rooms=0,
-        number_bathrooms=0,
-        max_guest=0,
-        price_by_night=0,
-        latitude=0.0,
-        longitude=0.0,
-        amenity_ids=[],
-        *args,
-        **kwargs
-    ):
-        """Init new Place """
-        super().__init__(**kwargs)
+    __tablename__ = "places"
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, default=0)
+    number_bathrooms = Column(Integer, default=0)
+    max_guest = Column(Integer, default=0)
+    price_by_night = Column(Integer, default=0)
+    latitude = Column(Float)
+    longitude = Column(Float)
+    reviews = relationship("Review", backref="place", cascade="delete")
 
-        self.city_id = city_id
-        self.user_id = user_id
-        self.name = name
-        self.description = description
-        self.number_rooms = number_rooms
-        self.number_bathrooms = number_bathrooms
-        self.max_guest = max_guest
-        self.price_by_night = price_by_night
-        self.latitude = latitude
-        self.longitude = longitude
-        self.amenity_ids = amenity_ids
+    if getenv("HBNB_TYPE_STORAGE", None) != "db":
+        @property
+        def reviews(self):
+            """list of all linked Reviews"""
+            from models import storage
+
+            rev_list = []
+            for rev in list(storage.all(Review).values()):
+                if rev.place_id == self.id:
+                    rev_list.append(rev)
+            return rev_list

1759e8e0426c23af8c31965116999bf560a6ea13
Salsabil <salsabeelahmed323@gmail.com>
2024-04-17 14:05:36 +0200

    pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -7,6 +7,7 @@ from sqlalchemy import Column, String, DATETIME
 
 Base = declarative_base()
 
+
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), unique=True, nullable=False, primary_key=True)
--- models/state.py
+++ models/state.py
@@ -7,13 +7,14 @@ from os import getenv
 import models
 from models.city import City
 
+
 class State(BaseModel, Base):
     """State class"""
 
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship("City",  backref="state", cascade="delete")
-    
+
     if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):

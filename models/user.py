c6c3e5b6f247373ea022c541b9fa15b6671fae55
Salsabil <salsabeelahmed323@gmail.com>
2024-04-17 14:33:55 +0200

    7. DBStorage - User

--- models/user.py
+++ models/user.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
 
-    def __init__(
-        self, email="",
-        password="", first_name="", last_name="", *args, **kwarg
-    ):
-        """Init new User"""
-        super().__init__(**kwarg)
-        self.email = email
-        self.password = password
-        self.first_name = first_name
-        self.last_name = last_name
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))

29440c68084e2dfaef5440cae8e7e8f7b3f8f31c
Salsabil <salsabeelahmed323@gmail.com>
2024-04-17 14:37:56 +0200

    7. DBStorage - User test db

--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+import os
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 
@@ -8,7 +9,8 @@ class User(BaseModel, Base):
     """This class defines a user by various attributes"""
 
     __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128))
+        last_name = Column(String(128))

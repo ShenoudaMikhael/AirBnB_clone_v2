bb78044ef8409911b5b358945e550d21e6e81961
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-04-18 03:29:59 +0200

    Fix clean

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base, Column, String
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
@@ -9,3 +10,5 @@ class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
 
     name = Column(String(128), nullable=False)
+    place_amenities = relationship(
+        "Place", secondary="place_amenity", viewonly=False)

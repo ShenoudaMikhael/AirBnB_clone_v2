61c99b333173cd41e0eaefc19b9275980c7bb156
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-05-04 00:53:48 +0300

    2. Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,7 @@ import fabric.api as fab
 def do_deploy(archive_path):
     """web static deploy"""
     # env.hosts = ["34.201.165.130", "34.224.62.173"]
-    fab.env.hosts = ["34.201.165.130"]
+    fab.env.hosts = "34.201.165.130"
     fab.env.user = "ubuntu"
     file_name = archive_path.split("/")[-1]
     file_name_dir = file_name.split(".")[0]
@@ -28,8 +28,7 @@ def do_deploy(archive_path):
         d1 = "/data/web_static/releases/{}/web_static/*".format(file_name_dir)
         d2 = "/data/web_static/releases/{}/".format(file_name_dir)
         fab.run("mv {} {}".format(d1, d2))
-        fab.run("rm -rf /data/web_static/releases/{}/web_static".format(
-            file_name_dir))
+        fab.run("rm -rf /data/web_static/releases/{}/web_static".format(file_name_dir))
         l1 = "/data/web_static/releases/{}/".format(file_name_dir)
         lc = "/data/web_static/current"
         fab.run("rm -rf {}".format(lc))

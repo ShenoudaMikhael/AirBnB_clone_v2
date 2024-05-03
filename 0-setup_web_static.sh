041439a28fe11b0a9e885316d0fdf6db4757a5a6
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-05-03 21:01:27 +0300

    0. Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,43 +14,45 @@ sudo mkdir -p /data/web_static/shared/
 
 # Create a fake HTML file /data/web_static/releases/test/index.html
 # (with simple content, to test your Nginx configuration)
-sudo echo "<html>
+echo "<html>
   <head>
   </head>
   <body>
-    Holberton School
+	Holberton School
   </body>
-</html>" | tee /data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html
 # Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder.
 # If the symbolic link already exists, it should be deleted and recreated every time the script is ran.
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 # Give ownership of the /data/ folder to the ubuntu user AND group (you can assume this user and group exist).
 # This should be recursive; everything inside should be created/owned by this user/group.
-sudo chown -R ubuntu:ubuntu /data/
+sudo chown -hR ubuntu:ubuntu /data
 
 echo "server {
 
-  	listen 80 default_server;
+	listen 80 default_server;
 	listen [::]:80 default_server;
-	root /var/www/html;
 	index index.html index.htm index.nginx-debian.html;
 	server_name _;
-		add_header X-Served-By 530990-web-02;
-	location /hbnb_static/ {
-    	alias /data/web_static/current/;
-  	}	
-  	location / {
-		try_files \$uri \$uri/ =404;
-	}
+	add_header X-Served-By \$hostname;
+
+
+
+	error_page 404 /404.html;
 	if (\$request_filename ~ redirect_me){
 		rewrite ^ https://thedreamcatcher.dev permanent;
 	}
-	error_page 404 /404.html;
-  	location = /404.html {
+	location = /404.html {
+		root /usr/share/nginx/html;
 		internal;
 	}
-
-  
+	location / {
+		root /var/www/html/;
+		try_files \$uri \$uri/ =404;
+	}
+	location /hbnb_static/ {
+		alias /data/web_static/current/;
+	}	
 }" | sudo tee /etc/nginx/sites-available/default
-
+sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
 sudo service nginx restart

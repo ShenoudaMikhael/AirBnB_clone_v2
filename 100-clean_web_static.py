7fa72f1ecc82f1f09236ef711f1870eca77dfc3e
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-05-04 04:47:47 +0300

    4. Keep it clean!

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,16 +8,10 @@ env.hosts = ["34.201.165.130", "34.224.62.173"]
 def do_clean(number=0):
     """do clean function"""
 
-    arch_count = int(number)
+    num = int(number)
 
-    if arch_count == 0 or arch_count == 1:
-        arch_count = 1
-
-    else:
-        arch_count += 1
-    local_dir = "cd versions/;"
-    remote_dir = "cd /data/web_static/releases/;"
-    comand = f"ls -t | tail -n +{arch_count} | xargs rm -rf"
-
-    local("{} {}".format(local_dir, comand))
-    sudo("{} {}".format(remote_dir, comand))
+    num = 2 if num == 0 else num + 1
+    
+    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(num))
+    path = '/data/web_static/releases'
+    sudo('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, num))

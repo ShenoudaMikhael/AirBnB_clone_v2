65bcab94677f4c8197f375ff62d53c73808fbfa1
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-05-04 04:00:16 +0300

    4. Keep it clean!

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """do clean module """
-import os
 from fabric.api import env, sudo, local
 
 env.hosts = ["34.201.165.130", "34.224.62.173"]
@@ -11,31 +10,14 @@ def do_clean(number=0):
 
     arch_count = int(number)
 
-    # if arch_count == 0 or arch_count == 1:
-    #     arch_count = 1
+    if arch_count == 0 or arch_count == 1:
+        arch_count = 1
 
-    # files = [
-    #     f for f in os.listdir("versions")
-    #     if os.path.isfile(os.path.join("versions", f))
-    # ]
+    else:
+        arch_count += 1
+    local_dir = "cd versions/;"
+    remote_dir = "cd /data/web_static/releases/;"
+    comand = f"ls -t | tail -n +{arch_count} | xargs rm -rf"
 
-    # files_to_remove = files[: arch_count * -2]
-
-    # for file in files_to_remove:
-    #     local("rm {}".format(os.path.join("versions", file)))
-    #     os.remove()
-
-    remote_dir = "/data/web_static/releases"
-    local_dir = "versions"
-    files = sudo(f"ls -t {remote_dir}").stdout.strip().split(" ")
-    print(files)
-    for file in files[arch_count:]:
-        print(file)
-        try:
-            sudo(f"rm -R {remote_dir}/{file}")
-        except Exception:
-            pass
-        try:
-            local(f"rm {local_dir}/{file}.tgz")
-        except Exception:
-            pass
+    local("{} {}".format(local_dir, comand))
+    sudo("{} {}".format(remote_dir, comand))

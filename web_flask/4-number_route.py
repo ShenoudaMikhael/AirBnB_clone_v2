6611c96883b740d6a4964f3298a033529a4d2cf8
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-05-17 18:50:26 +0300

    4. Is it a number?

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -33,7 +33,7 @@ def hello_python(text):
 @app.route("/number/<int:n>", strict_slashes=False)
 def hello_number(n):
     """number route"""
-    return str(n)
+    return "{} is a number$".format(n)
 
 
 if __name__ == "__main__":

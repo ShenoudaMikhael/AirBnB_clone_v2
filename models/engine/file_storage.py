cb22943bdde57cf02e024a8ce1d77c8589789d67
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-05-18 19:52:07 +0300

    8. List of states

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,12 +12,10 @@ class FileStorage:
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         if cls:
-            qqq = {
-                k: v
-                for k, v in FileStorage.__objects.items()
-                if v.__class__ == cls and k != "_sa_instance_state"
+            return {
+                k: v for k, v in FileStorage.__objects.items()
+                if v.__class__ == cls
             }
-            return qqq
         return FileStorage.__objects
 
     def new(self, obj):
@@ -28,13 +26,9 @@ class FileStorage:
         """Saves storage dictionary to file"""
         with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
             temp = {}
-            for v in FileStorage.__objects.values():
-                if hasattr(v, "_sa_instance_state"):
-                    del v._sa_instance_state
             temp.update(FileStorage.__objects)
             for key, val in temp.items():
-                if key != "_sa_instance_state":
-                    temp[key] = val.to_dict()
+                temp[key] = val.to_dict()
             json.dump(temp, f)
 
     def reload(self):
@@ -68,7 +62,8 @@ class FileStorage:
     def delete(self, obj=None):
         """Delete based on obj or not"""
         if obj:
-            del FileStorage.__objects["{}.{}".format(obj.__class__.__name__, obj.id)]
+            del FileStorage.__objects[
+                "{}.{}".format(obj.__class__.__name__, obj.id)]
 
     def close(self):
         """Close Method"""

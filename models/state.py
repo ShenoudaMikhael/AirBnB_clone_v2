8f035d7db2ec68c652913d2254db79f341eaf70f
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-05-18 17:21:35 +0300

    8. List of states

--- models/state.py
+++ models/state.py
@@ -9,17 +9,10 @@ class State(BaseModel, Base):
     """State class"""
 
     __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
 
-    # def __init__(self, *args, name="", **kwargs):
-    #     """Init function"""
-
-    #     super().__init__(*args, **kwargs)
-    #     self.name = name
-
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state", cascade="all, delete")
-    else:
+    if getenv("HBNB_TYPE_STORAGE") != "db":
 
         @property
         def cities(self):

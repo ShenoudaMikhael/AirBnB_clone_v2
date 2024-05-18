4eb4af2c90d46e046bfd42b0765fc547a63270d8
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-05-18 16:55:51 +0300

    8. List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,50 +12,6 @@ def teardown_db(exception):
     storage.close()
 
 
-@app.route("/", strict_slashes=False)
-def hello_world():
-    """Hello HBNB! Route"""
-    return "Hello HBNB!"
-
-
-@app.route("/hbnb", strict_slashes=False)
-def hello_hbnb():
-    """HBNB route"""
-    return "HBNB"
-
-
-@app.route("/c/<text>", strict_slashes=False)
-def hello_c(text):
-    """C route"""
-    return "C {}".format(str(text).replace("_", " "))
-
-
-@app.route("/python", defaults={"text": "is cool"}, strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def hello_python(text):
-    """Python route"""
-    return "Python {}".format(str(text).replace("_", " "))
-
-
-@app.route("/number/<int:n>", strict_slashes=False)
-def hello_number(n):
-    """number route"""
-    return "{} is a number".format(n)
-
-
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def hello_template(n):
-    """number route"""
-    return render_template("5-number.html", my_number=n)
-
-
-@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
-def hello_odd_even(n):
-    """number route"""
-    what_is = "{} is {}".format(n, "even" if n % 2 == 0 else "odd")
-    return render_template("5-number.html", my_number=what_is)
-
-
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """number route"""
@@ -64,4 +20,4 @@ def states_list():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000, host="0.0.0.0")
+    app.run(debug=True)

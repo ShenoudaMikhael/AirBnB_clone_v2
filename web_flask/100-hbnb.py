fd8b8e814aaebc4913bafa06b001e2ef23484f0b
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-05-19 19:38:53 +0300

    done

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Flask App Module"""
+from flask import Flask, render_template
+from models import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+app = Flask(__name__)
+ex_models = {
+    "Amenity": Amenity,
+    "City": City,
+    "Place": Place,
+    "Review": Review,
+    "State": State,
+    "User": User,
+}
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """cities by states route"""
+    states = storage.all(ex_models["State"]).values()
+    amenity = storage.all(ex_models["Amenity"]).values()
+    places = storage.all(ex_models["Place"]).values()
+    return render_template(
+        "100-hbnb.html", states=states, amenity=amenity, places=places
+    )
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Closes the storage on teardown"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(port=5000, host="0.0.0.0")
